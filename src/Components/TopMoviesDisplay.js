011bfa80327850718d8fcf8f0da9c8447b0e4a94
Ian Chang <personal.ianchang@gmail.com>
2024-12-10 05:59:48 -0500

    wrapped topmoviedisplay in chakra

--- src/Components/TopMoviesDisplay.js
+++ src/Components/TopMoviesDisplay.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import './TopMoviesDisplay.css';
-import { Text } from '@chakra-ui/react';
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Text } from '@chakra-ui/react';
 
 function TopMoviesDisplay({ titles }) {
-    if (!titles || titles.length === 0) {
-        return <Text fontSize="lg" color="brand.darkGray">No similar movies yet. Please enter your prompt!</Text>;
-    }
-    else {
-        return (
-            <table>
-              <thead>
-                <tr>
-                  <th>Movie Title</th>
-                  <th>Director</th>
-                </tr>
-              </thead>
-              <tbody>
-              {titles.map((movie, index) => (
-                    <tr key={index}>
-                    <td>{movie.title}</td>
-                    <td>{movie.director}</td>
-                    </tr>
-                ))}
-              </tbody>
-            </table>
-          );  
-    }
-};
+  if (!titles || titles.length === 0) {
+    return (
+      <Box textAlign="center" mt={4}>
+        <Text fontSize="lg" color="gray.500">
+          No similar movies yet. Please enter your prompt!
+        </Text>
+      </Box>
+    );
+  } else {
+    return (
+      <Box borderWidth="1px" borderRadius="lg" p={4} mt={4} overflowX="auto">
+        <Text fontSize="xl" fontWeight="bold" mb={4} color="gray.700">
+          Top Similar Movies
+        </Text>
+        <Table variant="striped" colorScheme="gray" size="md">
+          <Thead>
+            <Tr>
+              <Th>Movie Title</Th>
+              <Th>Director</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {titles.map((movie, index) => (
+              <Tr key={index}>
+                <Td>{movie.title}</Td>
+                <Td>{movie.director}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    );
+  }
+}
 
 export default TopMoviesDisplay;
